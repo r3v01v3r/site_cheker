d03b4fe1f8fa046e5a8e370f5d82179a77de988a
idoziru <idoziru@gmail.com>
2014-01-23 13:57:13 +0400

    loging

--- sitecheker.py
+++ sitecheker.py
@@ -3,6 +3,7 @@
 import datetime
 import collections
 import os
+import sys
 from tools import Site, Cache, Difference, Twitter, Config
 
 twitter = Twitter()
@@ -18,8 +19,8 @@ print('console')
 # определяем первый это запуск скрипта или нет
 try:
     # не первый запуск
-    log = open('log', 'r', encoding='utf-8')
-    log = open('log', 'a', encoding='utf-8')
+    log = open('logs/main_log', 'r', encoding='utf-8')
+    log = open('logs/main_log', 'a', encoding='utf-8')
     log_date = '{0}\n\n'.format(datetime.datetime.now())
     log.write('\n')
     log.write('-' * 20)
@@ -27,7 +28,8 @@ try:
     log.write(log_date)
 except:
     # первый запуск
-    log = open('log', 'w', encoding='utf-8')
+    os.mkdir('logs', 0o777)
+    log = open('logs/main_log', 'w', encoding='utf-8')
     log.write('\n')
     log.write('-' * 20)
     log.write('\n')
@@ -41,19 +43,19 @@ except:
     pass
 
 for project_name in all_project_names:
-    twitter_msg = ''
+    log_msg = ''
     # если это первый запуск для сайта, то только создает кэш
     if site.is_it_first_load_of_project(project_name) == True:
         cache.make_site_cache(project_name)
-        twitter_msg += '[{0}]: \n'.format(project_name)
-        twitter_msg += 'first start of project\n\n'
-        log.write(twitter_msg)
+        log_msg += '[{0}]: \n'.format(project_name)
+        log_msg += 'first start of project\n\n'
+        log.write(log_msg)
     else:
-        twitter_msg += '[{0}]: \n'.format(project_name)
+        log_msg += '[{0}]: \n'.format(project_name)
         # site_status
         try:
             site_status = site.get_status_code_of_site(project_name)
-            twitter_msg += 'site_status={0}\n'.format(site_status)
+            log_msg += 'site_status={0}\n'.format(site_status)
         except:
             continue
 
@@ -64,9 +66,9 @@ for project_name in all_project_names:
                 hrefs_on_site = site.how_much_hrefs_on_site(project_name)
                 hrefs_in_cahe = cache.how_much_hrefs_in_cache(project_name)
                 if hrefs_on_site != hrefs_in_cahe:
-                    twitter_msg += 'href_counting=notOK\n'
+                    log_msg += 'href_counting=notOK\n'
                 else:
-                    twitter_msg += 'href_counting=OK\n'
+                    log_msg += 'href_counting=OK\n'
         except:
             continue
 
@@ -74,11 +76,10 @@ for project_name in all_project_names:
         try:
             hrefs_different = site.get_bool_parametr_from_projects_cfg(project_name, 'hrefs_different')
             new_hrefs_on_site = difference.find_new_hrefs_on_site(project_name)
-            print(new_hrefs_on_site)
             if new_hrefs_on_site != []:
-                twitter_msg += 'hrefs_different=notOK\n'
+                log_msg += 'hrefs_different=notOK\n'
             else:
-                    twitter_msg += 'hrefs_different=OK\n'
+                    log_msg += 'hrefs_different=OK\n'
         except:
             continue
 
@@ -88,17 +89,16 @@ for project_name in all_project_names:
             if different_rows == True:
                 different_rows_list = difference.row_difference_to_list(project_name)
                 if different_rows_list != []:
-                    print('different_rows_list', different_rows_list)
-                    twitter_msg += 'different_rows=notOK\n'.format()
-                    twitter_msg += '{0}\n'.format(different_rows_list)
+                    log_msg += 'different_rows=notOK\n'.format()
+                    log_msg += '{0}\n'.format(different_rows_list)
                 else:
-                    twitter_msg += 'different_rows=OK\n'
+                    log_msg += 'different_rows=OK\n'
         except:
             continue
         # logging
-        print(twitter_msg)
-        log_msg = twitter_msg
-        log.write(twitter_msg)
+        log_msg = log_msg
+        log.write(log_msg)
         log.write('\n\n')
 
 log.close()
+sys.exit()
